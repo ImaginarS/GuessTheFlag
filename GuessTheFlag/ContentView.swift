0d739eea11752c6532e87ee0cd57c685b802f0ed
ImaginarS <sandra.quel0714@gmail.com>
2021-02-01 22:04:45 -0500

    added user score and flag selection
    Added an @State property to store the user’s score and modified it when the user gets an answer right or wrong. Show the player’s current score in a label. When someone chooses the wrong flag, the message is displayed in the alert.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France","Germany","Ireland","Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...3)
-    
+    @State private var userScore = 0
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
@@ -38,11 +38,13 @@ struct ContentView: View {
                             .shadow(color: .black, radius:2)
                     }
                 }
+                Text("Score: \(userScore)")
+                    .foregroundColor(.white)
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")){
+            Alert(title: Text(scoreTitle), message: Text(""), dismissButton: .default(Text("Continue")){
                 self.askQuestion()
             })
         }
@@ -50,8 +52,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore = userScore + 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            userScore = userScore - 1
         }
         showingScore = true
     }
